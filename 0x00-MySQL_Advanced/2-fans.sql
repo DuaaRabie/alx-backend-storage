779871b0812866c45e4247ab17bc85be30554ff8
Duaa Rabie <DuaaRabie11@gmail.com>
2024-10-17 23:07:58 +0000

    task0

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,5 +2,9 @@
 -- Best band ever!
 
 SELECT origin, nb_fans
-FROM metal_bands
+FROM (
+    SELECT origin, COUNT(*) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+) ranked_countries
 ORDER BY nb_fans DESC;
