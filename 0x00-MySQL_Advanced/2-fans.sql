b9ff69328040af0595cc81fcee1e12a9e0573ce1
Duaa Rabie <DuaaRabie11@gmail.com>
2024-10-17 23:09:35 +0000

    task0

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,10 +1,13 @@
 -- task2
 -- Best band ever!
 
-SELECT origin, nb_fans
-FROM (
+WITH ranked_countries AS (
     SELECT origin, COUNT(*) AS nb_fans
     FROM metal_bands
     GROUP BY origin
-) ranked_countries
+)
+SELECT origin, nb_fans,
+       RANK() OVER (ORDER BY nb_fans DESC) AS rank
+FROM ranked_countries
 ORDER BY nb_fans DESC;
+
