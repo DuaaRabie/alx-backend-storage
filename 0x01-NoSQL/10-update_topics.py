188eabed29b0291b5a031113c6e46c2b563f4958
Duaa Rabie <DuaaRabie11@gmail.com>
2024-10-22 16:24:47 -0700

    update code

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,6 +3,7 @@
 
 
 def update_topics(mongo_collection, name, topics):
+    """ updaet topics """
     mongo_collection.update(
             { "name": name }
             { "$set":{ "topics": topics } }
