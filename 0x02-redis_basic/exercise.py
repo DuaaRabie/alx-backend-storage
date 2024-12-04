7430d117866c7aba56aad2958d3a1727eaeec8a4
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-04 20:16:38 +0000

    task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -18,3 +18,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]) ->\
+            Union[bytes, str, int, float, None]:
+        """ convert data to desired format using Callable """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ Retrieve the value and decode it to a string """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """Retrieve the value and decode it to an integer """
+        return self.get(key, fn=lambda d: int(d))
